465938400e5bd9d0384050fbdbfa27b367c11328
rmishra01-ui <rmishra01@founderstxstudents.org>
2026-03-12 16:15:49 -0500

    Add initial set definitions and print statements
    Initialize sets and demonstrate basic operations.

---
+++ operationInSets.py
@@ -0,0 +1,14 @@
+hello = {1, 2, 3, 4, 5}
+print(hello)
+
+world = {"helloworld", 4, 4.67, True}
+print(world)
+
+set1 = {1, 2, 3, 4, 4, 5}
+print(set1)
+
+set2 = {1, 2, 3, 4, 5, {6, 7, 8, 9, 10}}
+print(set2)
+
+set3 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+print(set3.pop())

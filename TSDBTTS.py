4e4889f3a0ad250e84ef6247c426f2ad434444e7
rmishra01-ui <rmishra01@founderstxstudents.org>
2026-03-19 14:50:51 -0500

    Implement symmetric difference examples in TSDBTTS.py
    Add example code for symmetric difference in sets

---
+++ TSDBTTS.py
@@ -0,0 +1,13 @@
+'''
+This is all about the symmetric difference between the two sets, in Python.
+'''
+
+# Part 1
+set1 = {"blue", "green"}
+set2 = {"blue", "yellow"}
+print(set1.symmetric_difference(set2))
+
+# Part 2
+set3 = {1, 2, 3, 4, 5}
+set4 = {1, 5, 6, 7, 8, 9,}
+print(set3.symmetric_difference(set4))

b2e73aa03a143cc97490216aad238436791a7aef
rmishra01-ui <rmishra01@founderstxstudents.org>
2026-03-19 15:32:03 -0500

    "This is The End"
    Imagine that someone named Jack just accidently, or unexpectedly typed the word EXIT, in Python. This project tells Jack that the list stops when I is equal to 5. This is the list with 10 numbers, ranging from 1 to 10.

---
+++ TheEnd.py
@@ -0,0 +1,6 @@
+# Imagine that someone just accidently, or unexpectedly typed the word EXIT, in Python. This project tells that the list stops when I is equal to 5. This is the list with 10 numbers, ranging from 1 to 10.
+for i in range(10):
+  if i == 5:
+    print(exit)
+    exit = exit()
+print(i)
